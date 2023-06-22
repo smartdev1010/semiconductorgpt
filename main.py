1ba45ba84d6bbd513f8900d7ecf2d736ba09aab4
smartdev1010 <shvab.serhii86@gmail.com>
2023-06-22 22:30:52 +0300

    permanenet input recursive

--- main.py
+++ main.py
@@ -24,28 +24,6 @@ vectorstore = FAISS.load_local("./vector", embeddings=OpenAIEmbeddings())
 query = "Give me a perspective on the Semiconductor industry. What is the future outlook of this industry? What are the drivers of this market? Tell me what capabilities does a player need to win in this market? Keep in mind the global interconnecting nature of the players and countries in this industry."
 
 qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0), vectorstore.as_retriever(), memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True))
-result = qa({"question": query})
-answer1 = result["answer"]
-
-print("\nAnswer based on Docs\n")
-print("\n=====================================================================================\n")
-print(answer1)
-print("\n=====================================================================================\n")
-
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo-16k",
-  messages=[
-    {"role": "system", "content": "You are a McKinsey partner who is known for his cutting edge insights. You are consulting a client who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information. ."},
-    {"role": "user", "content": query}
-  ],
-  
-)
-answer2 = completion.choices[0].message.content
-
-print("\nChatGPT answer\n")
-print("\n=====================================================================================\n")
-print(answer2)
-print("\n=====================================================================================\n")
 
 prompt_template = """You are You are a McKinsey partner who is known for his cutting edge insights. The stakes are high & you are consulting a client who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information. 
 
@@ -58,9 +36,36 @@ If first optional answer is along the lines of 'I don't know' then just return s
 If not, please combine & synthesize both the answers in a way that the context from both the answers is maintained and return the combined insights as the final answer."""
 
 llm_chain = LLMChain(llm=ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0, max_tokens=7500), prompt=PromptTemplate.from_template(prompt_template))
-final = llm_chain.run(query=query, answer1=answer1, answer2=answer2)
 
-print("\Final answer\n")
-print("\n=====================================================================================\n")
-print(final)
-print("\n=====================================================================================\n")
+while True:
+  query = input("Input your query: ")
+
+  result = qa({"question": query})
+  answer1 = result["answer"]
+
+  print("\nAnswer based on Docs\n")
+  print("\n=====================================================================================\n")
+  print(answer1)
+  print("\n=====================================================================================\n")
+
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-16k",
+    messages=[
+      {"role": "system", "content": "You are a McKinsey partner who is known for his cutting edge insights. You are consulting a client who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information. ."},
+      {"role": "user", "content": query}
+    ],
+    
+  )
+  answer2 = completion.choices[0].message.content
+
+  print("\nChatGPT answer\n")
+  print("\n=====================================================================================\n")
+  print(answer2)
+  print("\n=====================================================================================\n")
+  
+  final = llm_chain.run(query=query, answer1=answer1, answer2=answer2)
+
+  print("\Final answer\n")
+  print("\n=====================================================================================\n")
+  print(final)
+  print("\n=====================================================================================\n")
