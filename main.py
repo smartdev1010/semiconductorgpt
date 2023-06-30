276075c7f4793633303c3a9a298a27ee0b2a8f36
smartdev1010 <shvab.serhii86@gmail.com>
2023-07-01 02:21:47 +0300

    automate

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ import asyncio
 import pinecone
 import openai
 import os
+import json
 from dotenv import load_dotenv
+import pandas as pd
 
 
 load_dotenv()
@@ -18,13 +20,14 @@ ans_chatgpt = ""
 ans_docqa = ""
 ans_googlegpt = ""
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 async def chatgpt(query: str):
     print("\nChatGPT answer\n")
     print(
         "\n=====================================================================================\n"
     )
-    openai.api_key = os.getenv("OPENAI_API_KEY")
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         temperature=0,
@@ -128,27 +131,112 @@ if __name__ == "__main__":
     while True:
         query = input("Input your query: ")
 
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main(query))
+        # functions = [
+        #     {
+        #         "name": "check_and_run_visualization",
+        #         "description": "Check message requires graph and run proper actions.",
+        #         "parameters": {
+        #             "type": "object",
+        #             "properties": {
+        #                 "graph_required": {
+        #                     "type": "boolean",
+        #                     "description": "Check message requires graph and run proper actions. If required, true else, false.",
+        #                 }
+        #             },
+        #             "required": ["graph_required"],
+        #         },
+        #     }
+        # ]
 
-        completion = openai.ChatCompletion.create(
+        response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             temperature=0,
             messages=[
                 {
                     "role": "system",
-                    "content": "You are You are a McKinsey partner who is known for his cutting edge insights. The stakes are high & you are consulting a client who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information. DON'T mention you are combining several source of information in the answer.",
+                    "content": "You are a helpful AI Bot that determines given query requires graph visualization answer. For example, if query asks you to compare companies financial data, it requires graph visualization. Only return 'yes' or 'no' depends on it requires graph.",
                 },
-                {
-                    "role": "user",
-                    "content": f"Question is {query}.\nYou have two answers to pick from as sources of insights .\nFirst optional answer is {ans_chatgpt}. \nSecond optional answer is {ans_docqa}.\nThird optional answer is {ans_googlegpt}\n\nPlease combine & synthesize all the answers in a way that the context from all the answers is maintained and return the combined insights as the final answer.",
-                },
+                {"role": "user", "content": f"This is query: {query}."},
             ],
+            # functions=functions,
+            # function_call="auto",
         )
-        ans_final = completion.choices[0].message.content
 
-        print(ans_final)
+        message = response["choices"][0]["message"]
+        print(message)
 
-        print(
-            "\n=====================================================================================\n"
-        )
+        if message.content == "No":
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main(query))
+
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-16k",
+                temperature=0,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are You are a McKinsey partner who is known for his cutting edge insights. The stakes are high & you are consulting a client who is going to give you a 100 million contract if you are insightful enough. You always give a so-what to the client when providing facts. You never give random answers that have no meaning and you are always focused on nuanced insights combining multiple legitimate sources of information. DON'T mention you are combining several source of information in the answer.",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Question is {query}.\nYou have two answers to pick from as sources of insights .\nFirst optional answer is {ans_chatgpt}. \nSecond optional answer is {ans_docqa}.\nThird optional answer is {ans_googlegpt}\n\nPlease combine & synthesize all the answers in a way that the context from all the answers is maintained and return the combined insights as the final answer.",
+                    },
+                ],
+            )
+            ans_final = completion.choices[0].message.content
+
+            print(ans_final)
+
+            print(
+                "\n=====================================================================================\n"
+            )
+        else:
+            df = pd.read_excel("./xlsxs/Merged data.xlsx")
+            df = df.astype(str)
+            xlsx_string = df.to_csv(index=False, sep="\t")
+
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-16k",
+                temperature=0,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a McKinsey consultant analyzing financial information.",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Following is semiconductor companies financial tabular information.\n\n {xlsx_string} \n\n {query}",
+                    },
+                ],
+            )
+
+            anaylze = completion["choices"][0]["message"].content
+
+            print(anaylze)
+
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-16k",
+                temperature=0,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a McKinsey consultant analyzing financial information.",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Following is analyzed financial information.\n\n {anaylze} \n\n Give me python code that visualize above information by using matplotlib library. Only return code.",
+                    },
+                ],
+            )
+
+            code = completion["choices"][0]["message"].content
+
+            code = code.replace("python", "")
+            if "```" in code:
+                extract = code.split("```")[1]
+            else:
+                extract = code
+
+            print(extract)
+
+            exec(extract)
